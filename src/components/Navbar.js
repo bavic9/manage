b39600dc5f2728a70bd5b6dac8d6def80711d6ef
Bavic <bavic97@gmail.com>
2023-10-27 23:36:16 +0100

    fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import logo from '../img/logo.svg'
 import { NavLink } from 'react-bootstrap';
 
@@ -11,16 +11,26 @@ const Navbar = () => {
         navRef.current.classList.toggle('open')
     }
 
+    const [menuOpened, setMenuOpened] = useState(false)
+
+    const getMenuStyles = (menuOpened) => {
+        if (document.documentElement.clientWidth <= 800)
+        {
+            return {right: !menuOpened && '-100%'}
+        }
+    }
 
 
 
   return (
     <nav className='relative container mx-auto p-6'>
-        <div ref={navRef} className='flex item-center justify-between'>
+        <div ref={navRef} className='flex item-center justify-between relative'>
             <div className='pt-2'>
                 <img src={logo} alt={logo}/>
             </div>
-            <div className='hidden md:flex space-x-6'>
+            <div
+            style={getMenuStyles(menuOpened)}
+            className='md:flex float-right md:space-x-6 space-y-4 md:space-y-0  menu'>
                 <a href='#' className='hover:text-darkGrayishBlue'>Pricing</a>
                 <a href='#' className='hover:text-darkGrayishBlue'>Product</a>
                 <a href='#' className='hover:text-darkGrayishBlue'>About</a>
@@ -31,19 +41,21 @@ const Navbar = () => {
                 <a href='#' className='hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight'>Get Started</a>
             </button>
 
-            <button id='menu-btn' onClick={showNav}  className=' block hamburger md:hidden focus:outline-none'>
-                <span className='hamburger-top'></span>
-                <span className='hamburger-middle'></span>
-                <span className='hamburger-bottom'></span>
-            </button>
+            <div onClick={ () => setMenuOpened ((prev) => !prev)}>
+                <button id='menu-btn' onClick={showNav} className=' block hamburger md:hidden focus:outline-none'>
+                    <span className='hamburger-top'></span>
+                    <span className='hamburger-middle'></span>
+                    <span className='hamburger-bottom'></span>
+                </button>
+            </div>
 
-            <div id='menu' onClick={showNav} className='hidden menuLink absolute flex flex-col items-center self-end  py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 top-20 right-6 drop-shadow-md'>
+            {/* <div id='menu' onClick={showNav} className='hidden menuLink absolute flex flex-col items-center self-end  py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 top-20 right-6 drop-shadow-md'>
                 <a className='link' href="#">Pricing</a>
                 <a className='link' href="#">Product</a>
                 <a className='link' href="#">About Us</a>
                 <a className='link' href="#">Careers</a>
                 <a className='link' href="#">Community</a>
-            </div>
+            </div> */}
         </div> 
         
     </nav>
@@ -51,3 +63,6 @@ const Navbar = () => {
 } 
 
 export default Navbar
+
+
+
